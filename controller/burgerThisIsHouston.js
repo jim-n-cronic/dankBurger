6efa7cb649ceeabbdb16ac5d4fcb4119adb49257
James Appello <jamesappello@gmail.com>
2019-10-15 14:00:13 -0400

    file setup

---
+++ controller/burgerThisIsHouston.js
@@ -0,0 +1,23 @@
+// express
+var express = require('express');
+// router
+var router = express.Router();
+
+// REQUIRE IMPORTED MODEL (burger.js) to use the db-functionality
+
+/**
+ * 
+ // create all routes and 
+ set up logic within those routes 
+ * \/\/\/\/~~ BELOW ~~\/\/\/\/
+ */
+//router.get
+
+//router.post
+
+//router.put (and .update)
+
+//router.delete
+
+//======================
+// EXPORT ROUTER (router)
---
+++ models/burger.js
@@ -0,0 +1,8 @@
+// IMPORT ORM from config folder
+
+// define burger variable
+
+
+
+
+// export the burger varubale
